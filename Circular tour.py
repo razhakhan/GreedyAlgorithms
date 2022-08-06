34550b0eb8a3c9c283bd0e6ede3e212cf49a99c6
Razha Khan <65012840+razhakhan@users.noreply.github.com>
2022-08-06 17:22:15 +0530

    Create Circular tour.py

---
+++ Circular tour.py
@@ -0,0 +1,70 @@
+'''
+    lis[][0]:Petrol
+    lis[][1]:Distance
+'''
+
+class Solution:
+    
+    #Function to find starting point where the truck can start to get through
+    #the complete circle without exhausting its petrol in between.
+    def tour(self,lis, n):
+        totalpetrol=0
+        totaldistance=0
+        for i in lis:
+            totalpetrol+=i[0]
+            totaldistance+=i[1]
+        
+        # distance more than petrol travel not possible    
+        if(totaldistance>totalpetrol):
+            return -1
+            
+        ans=0    
+        max2=0  # can be treated as petrol in tank
+        # kadane's algo modification
+        for i in range(n):
+            max2+=(lis[i][0]-lis[i][1])
+                
+            if(max2<0):
+                ans=i+1 # starting point will be next index
+                max2=0  # start with empty tank
+        return ans
+            
+#{ 
+ # Driver Code Starts
+if __name__ == '__main__': 
+    t = int(input())
+    for i in range(t):
+        n = int(input())
+        arr=list(map(int, input().strip().split()))
+        lis=[]
+        for i in range(1, 2*n, 2):
+            lis.append([ arr[i-1], arr[i] ])
+        print(Solution().tour(lis, n))
+    # Contributed by: Harshit Sidhwa
+# } Driver Code Ends
+
+
+'''
+
+Suppose there is a circle. There are N petrol pumps on that circle. You will be given two sets of data.
+1. The amount of petrol that every petrol pump has.
+2. Distance from that petrol pump to the next petrol pump.
+Find a starting point where the truck can start to get through the complete circle without exhausting its petrol in between.
+Note :  Assume for 1 litre petrol, the truck can go 1 unit of distance.
+
+Example 1:
+
+Input:
+N = 4
+Petrol = 4 6 7 4
+Distance = 6 5 3 5
+Output: 1
+Explanation: There are 4 petrol pumps with
+amount of petrol and distance to next
+petrol pump value pairs as {4, 6}, {6, 5},
+{7, 3} and {4, 5}. The first point from
+where truck can make a circular tour is
+2nd petrol pump. Output in this case is 1
+(index of 2nd petrol pump).
+
+'''
